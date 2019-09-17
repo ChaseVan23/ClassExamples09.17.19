5553d9c0f73b0ee5d5afa97097e6724997f65f09
0774775 <0774775@KELCE-LABS17-P.pittstate.edu>
2019-09-17 11:13:17 -0500

    File examples

---
+++ mavenproject1/src/main/java/Examples.java
@@ -0,0 +1,33 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 0774775
+ */
+public class Examples {
+    public static void main(String[] args) throws FileNotFoundException {
+         System.out.println("Please enter file name");
+         
+         Scanner keyboard = new Scanner(System.in);
+         String fileName = keyboard.nextLine();
+         
+         File myFile = new File("C:\\users\\chasec\\desktop\\" + fileName);
+         Scanner fileScanner = new Scanner(myFile);
+         
+         int counter = 0;
+         while(fileScanner.hasNext() && counter < 5) {
+             System.out.println(fileScanner.nextLine());
+             counter++;
+         }
+         
+    }
+}
